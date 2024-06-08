663c2e09e0c6d9e824d76f5f95aba740994ca8b9
Matheus Cavalari <matheuscavbarbosa@hotmail.com>
2024-06-08 17:49:40 -0300

    Criação da tela que lista os produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './autenticacao/login/login.component';
+import { CadastroComponent } from './autenticacao/cadastro/cadastro.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent,
   },
+  {
+    path: 'cadastro',
+    component: CadastroComponent
+  }
 ];
 
 @NgModule({
