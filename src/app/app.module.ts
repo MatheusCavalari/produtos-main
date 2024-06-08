8d8fc89f17b43f4d59fd4b00ad28ae85487247e9
Matheus Cavalari <matheuscavbarbosa@hotmail.com>
2024-06-08 16:50:22 -0300

    Criação dos módulos: shared e material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './core/material.module';
+import { SharedModule } from './shared/header/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/header/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { MaterialModule } from "src/app/core/material.module";
+import { HeaderComponent } from "./header.component";
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+
+})
+
+export class SharedModule{ }
